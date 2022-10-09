623747882638c42383b353ac8a08dd5295d4b18f
mnadimum <112414182+mnadimum@users.noreply.github.com>
2022-10-09 15:40:40 +0600

    password_generator

---
+++ password_generator.py
@@ -0,0 +1,20 @@
+import random
+
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_+*/-."
+
+while True:
+	pass_length = int(input("Enter the length of required password: "))
+	pass_count = int(input("Enter the number of required password: "))
+
+	for i in range(0, pass_count):
+		password = ""
+		for j in range(0, pass_length):
+			pass_char = random.choice(characters)
+			password = password + pass_char
+		
+		print("The generated password is", password)
+	repeat = input("Do you want to generate more passwords? ")
+	if repeat =="no" or repeat == "No":
+		break	
+print("Thank you!!")
+		
